f9586960133f4709cca243c1d7913f07383491d4
ciceromayk <38115321+ciceromayk@users.noreply.github.com>
2025-08-05 08:34:01 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime, LargeBinary, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
@@ -9,21 +9,33 @@ class Terreno(Base):
     
     id = Column(Integer, primary_key=True, index=True)
     data_avaliacao = Column(DateTime, default=datetime.utcnow)
-    # Critérios Jurídicos
+    
+    # Dados do Terreno
+    descricao = Column(String)
+    endereco = Column(String)
+    street_view_img = Column(LargeBinary)  # Armazena a imagem em formato binário
+    area_terreno = Column(Float)
+    altura_maxima = Column(Float)
+    lenol_freatico_permite_subsolo = Column(Boolean)
+    responsavel = Column(String)
+    
+    # Critérios Jurídicos (20%)
     doc_regular = Column(Integer)
     ausencia_onus = Column(Integer)
     potencial_aprovacao = Column(Integer)
-    # Critérios Físicos
+    
+    # Critérios Físicos (30%)
     area_dimensoes = Column(Integer)
     topografia = Column(Integer)
     infraestrutura = Column(Integer)
     zoneamento = Column(Integer)
-    # Critérios Comerciais
+    
+    # Critérios Comerciais (40%)
     localizacao = Column(Integer)
     estimativa_vgv = Column(Integer)
     demanda_concorrencia = Column(Integer)
-    # Alinhamento com o Produto
     adequacao_produto = Column(Integer)
-    # Resultado final
-    score = Column(Integer)  # Nota final (0 a 100)
-    selo = Column(String)    # SQI A, SQI B, etc.
+    
+    # Resultado Final
+    score = Column(Integer)
+    selo = Column(String)
